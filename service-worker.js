e717246d13c08f4acca2602023085375fb795896
businessappemail-bt <businessappemail@gmail.com>
2026-05-21 16:58:42 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,19 @@
-const CACHE_NAME = "taskmint-cache-v2";
-
-const filesToCache = [
-  "index.html",
-  "signup.html",
-  "login.html",
-  "how-it-works.html",
-  "manifest.json"
-];
+const CACHE_NAME = "taskmint-cache-v3";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(filesToCache);
-    })
-  );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
 
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
+  event.respondWith(fetch(event.request));
 });
